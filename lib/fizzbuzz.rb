0c6be78953f4d85cac94d2c162a5aa42062d4405
ElleMass <leah.massey@hotmail.co.uk>
2023-02-08 15:28:31 +0000

    test for multiple of 5 and multiple of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-      'fizz'
+  if num % 3 == 0 && num % 5 == 0 
+    'fizzbuzz'
+  elsif num % 3 == 0
+    'fizz'
+  elsif num % 5 == 0 
+    'buzz'
   end
 end 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,31 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
   it 'returns "fizz" when passed a multiple of 3' do
-    input = gets.chomp.to_i
-    expect(fizzbuzz(input)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
   end
+  
+  it 'returns "buzz" when passed a multiple of 5' do 
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
+  
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do 
+    expect(fizzbuzz(15)). to eq 'fizzbuzz'
+  end
+    
 end
+
+
+#where we were yesterday
+# require 'fizzbuzz'
+
+# describe 'fizzbuzz' do 
+#   input = gets.chomp.to_i
+#   it 'returns "fizz" when passed a multiple of 3' do
+#     expect(fizzbuzz(input)).to eq 'fizz'
+#   end
+  
+#   it 'returns "buzz" when passed a multiple of 5' do 
+#     expect(fizzbuzz(input)).to eq 'buzz'
+#   end
+    
+# end
